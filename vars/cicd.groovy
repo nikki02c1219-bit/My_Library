16880c9a548684e9832b0d52e758188b58b6df6c
nikki02c1219-bit <nikki02c1219@gmail.com>
2026-05-29 12:56:33 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,3 +6,7 @@ def gitbuild()
 {
   sh "mvn package"
 }
+def gitdeploy(jobname,ip,con)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${con}.war"
+}  
